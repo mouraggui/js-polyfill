9ee81249ae69bc1f8d240341d114a5889409bec9
Guilherme Moura <mouraggui@gmail.com>
2020-01-19 17:44:39 -0300

    adding some tests to Array.prototype.reduce

--- src/Array/prototype.reduce/reduce-test.js
+++ src/Array/prototype.reduce/reduce-test.js
@@ -1,6 +1,26 @@
 const test = require('tape')
 const reduce = require('./reduce')
 
+test('Array.prototype.reduce: Reducing array of numbers', assert => {
+  const numbers = [1, 2, 3, 4, 5]
+
+  const result = reduce.call(numbers, (acc, item) => acc + item, 0)
+  
+  assert.equal(result, 15)
+  assert.equal(typeof result, 'number')
+  assert.end()
+})
+
+test('Array.prototype.reduce: Reducing array of strings', assert => {
+  const letters = ['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']
+
+  const result = reduce.call(letters, (acc, item) => acc + item, '')
+  
+  assert.equal(result, 'JavaScript')
+  assert.equal(typeof result, 'string')
+  assert.end()
+})
+
 test('Array.prototype.reduce: Reducing an objects properties', assert => {
   const products = [
     { id: 1, price: 12.2 },
@@ -11,6 +31,7 @@ test('Array.prototype.reduce: Reducing an objects properties', assert => {
   const result = reduce.call(products, (acc, item) => acc + item.price, 0)
   
   assert.equal(result, 337.14)
+  assert.equal(typeof result, 'number')
   assert.end()
 })
 
@@ -24,6 +45,7 @@ test('Array.prototype.reduce: Omitting an initial value', assert => {
   const result = reduce.call(products, (acc, item) => acc + item)
   
   assert.equal(result, '[object Object][object Object][object Object]')
+  assert.equal(typeof result, 'string')
 
   assert.end()
 })
@@ -34,6 +56,17 @@ test('Array.prototype.reduce: Passing the callback function as null', assert => 
   } catch (err) {
     assert.assert(err instanceof TypeError)
     assert.equal(err.message, 'null is not a function')
+  }
+
+  assert.end()
+})
+
+test('Array.prototype.reduce: Calling the function without parameters', assert => {
+  try {
+    reduce.call([])
+  } catch (err) {
+    assert.assert(err instanceof TypeError)
+    assert.equal(err.message, 'undefined is not a function')
   }
 
   assert.end()
--- src/Array/prototype.reduce/reduce.js
+++ src/Array/prototype.reduce/reduce.js
@@ -2,11 +2,11 @@ module.exports = function (f, initial = this[0]) {
   if (typeof f !== 'function') throw TypeError(`${f} is not a function`)
 
   let acc = initial
-  let next = this[+(initial === this[0])]
+  let next = this[Number(initial === this[0])]
   
   for (let i = 0; i < this.length; i++) {
     acc = f(acc, next, i, this)
-    next = this[this.indexOf(next) + 1]
+    next = this[this.indexOf(next, i) + 1]
     if (next === undefined) break
   }
 
