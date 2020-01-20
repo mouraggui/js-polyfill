398d14396fcc367a555a4349afed00b71ccabc54
Guilherme Moura <mouraggui@gmail.com>
2020-01-20 11:09:27 -0300

    refactoring the reduce method for compatibility

--- src/Array/prototype.reduce/reduce.js
+++ src/Array/prototype.reduce/reduce.js
@@ -1,12 +1,13 @@
-module.exports = function (callback, initial = this[0]) {
+module.exports = function (callback, initial) {
   if (typeof callback !== 'function') {
     throw TypeError(`${callback} is not a function`)
   }
 
-  let acc = initial
-  let next = this[Number(initial === this[0])]
+  var acc = (initial !== undefined) ? initial : this[0]
+  var initialIndex = (initial !== undefined) ? 0 : 1
+  var next = this[initialIndex]
   
-  for (let i = 0; i < this.length; i++) {
+  for (var i = 0; i < this.length; i++) {
     acc = callback(acc, next, i, this)
     next = this[this.indexOf(next, i) + 1]
     if (next === undefined) break
